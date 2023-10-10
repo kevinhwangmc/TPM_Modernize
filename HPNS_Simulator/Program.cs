3e3dad9b0484c649f4cf03b677136d22a6aa76ef
kevinhwangmc <107270443+kevinhwangmc@users.noreply.github.com>
2023-10-10 10:23:59 -0400

    commit

--- HPNS_Simulator/Program.cs
+++ HPNS_Simulator/Program.cs
@@ -1,15 +1,12 @@
-﻿using HPNSDataGenerator;
-using HPNSDataGenerator.util;
-
-AnalysisTblV3 data = new AnalysisTblV3();
-var dd = data.GetBytes();
-
+﻿
 Thread t = new Thread(delegate ()
 {
     // replace the IP with your system IP Address...
     //Server myserver = new Server("172.20.10.2", 13000);
     //Server myserver = new Server("192.168.1.73", 13000);
-    Server myserver = new Server("192.168.1.6", 13000);
+    //Server myserver = new Server("192.168.1.6", 13000);
+    Server myserver = new Server("192.168.219.112", 13000);
+    
 });
 t.Start();
 
